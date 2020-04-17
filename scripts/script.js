de6c4adbef6243253037b15c4e02eb8a2706568c
Soliman-beep <60599592+Soliman-beep@users.noreply.github.com>
2020-04-17 03:42:18 +0200

    :poop: Grid + Hamburger
    Grid + Hamburger start, needs to be improved

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const topNav = document.querySelector("#myTopnav");
+    if (topNav.className === "topnav") {
+      topNav.className += " responsive";
+    } else {
+      topNav.className = "topnav";
+    }
+}

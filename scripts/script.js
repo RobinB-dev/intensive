47221530059ec6148bdafae703b954326435c5bc
Soliman-beep <60599592+Soliman-beep@users.noreply.github.com>
2020-04-21 14:00:54 +0200

    :construction:  Cards JS
    Cards JS

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,12 @@ confirmButton.addEventListener('click', (event) =>
 })
 
 const deck = [200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225]
-
 const randomCard = Math.ceil(Math.random()*26)
-
 const deckCard = document.querySelector('.card')
-
 deckCard.src = deck [randomCard-1]
+
+const indexCard = indexOf(deckCard.src)
 console.log(deck.indexOf(deckCard.src))
-deck.splice(deck.indexOf(deckCard.src),1)
+deck.splice(indexCard,1)
 console.log(deckCard.src)
 console.log(deck)

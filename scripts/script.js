a6696202404acb673c94e96fff3a8451a2e8fcdf
Umb3rs <60504892+Umb3rs@users.noreply.github.com>
2020-04-21 11:06:08 +0200

    :construction:
    update js for deck card

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@
 let playerName = document.querySelector('.player_4')
 const inputName = document.querySelector('.inputName')
 const confirmButton = document.querySelector('.confirmButton')
-let playerPseudo = document.querySelector('.playerPseudo')
+const playerPseudo = document.querySelector('.playerPseudo')
 
 // When click, place player name in his place and hides the question
 confirmButton.addEventListener('click', (event) => 
@@ -11,3 +11,14 @@ confirmButton.addEventListener('click', (event) =>
   playerName.textContent = inputName.value
   playerPseudo.style.display = 'none'
 })
+
+const deck = [200,200,200,200,100,100,100,100,100,100,100,100,100,100,100,100,75,75,75,75,75,75,75,75,75,75]
+
+const randowmCard = Math.ceil(Math.random()*26)
+
+const deckCard = document.querySelector('.card')
+
+
+deckCard.src = deck [randowmCard-1]
+
+console.log(deckCard.src)

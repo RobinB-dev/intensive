13bc4c840928438d45be9ea8368af796f49d022e
Robin BOURACHOT <robin.bourachot@hetic.net>
2020-04-22 23:12:34 +0200

    :rocket: Cards positioning
    Cards positioning

--- scripts/scriptJS.js
+++ scripts/scriptJS.js
@@ -54,6 +54,7 @@ let card6 = 0
 function takeCard() {
   const card = deck.pop()
   hand.push(card)
+
   // to pick up a random card from the deck and put it in the hand
   // hand.splice(0,0,deck [Math.floor(Math.random()*deck.length)])
   // o++
@@ -63,26 +64,31 @@ function takeCard() {
   const name = [`./images/cards/${nick[0]}.svg`]
   const image = name
 
+
   // if it's true the card goes in the hand
   if (card1 == 0) {
-    console.log(card1);
-    
+    console.log(hand);
+    hand.splice(0, 1, card);
     document.querySelector('.card1').src = image
     card1 = 1    
-    console.log(card1);
   } else if (card2 == 0) {
+    hand.splice(1, 1, card);
     document.querySelector('.card2').src = image
     card2 = 1    
   } else if (card3 == 0) {
+    hand.splice(2, 1, card);
     document.querySelector('.card3').src = image
     card3 = 1    
   } else if (card4 == 0) {
+    hand.splice(3, 1, card);
     document.querySelector('.card4').src = image
     card4 = 1    
   } else if (card5 == 0) {
+    hand.splice(4, 1, card);
     document.querySelector('.card5').src = image
     card5 = 1    
   } else if (card6 == 0) {
+    hand.splice(5, 1, card);
     document.querySelector('.card6').src = image
     card6 = 1    
   }
@@ -95,15 +101,15 @@ const moveCard = document.querySelectorAll('.myCards')
 for (card of moveCard){ 
   card.addEventListener('click', moveCardFunc)
 }
+let placeType = ''
 function moveCardFunc() {
   let getIndex = this.classList[1].substr(4)
   // console.log(hand)
   const nick = hand.map(hand => `${hand.nick}`)
   const type = hand.map(hand => `${hand.type}`)
   const moveSRC = [`./images/cards/${nick[parseInt(getIndex-1)]}.svg`]
-  const placeType = '.' + type[0]
-  // console.log(placeType)  
-  document.querySelector(placeType).src = moveSRC
+  console.log(type)
+  
   this.src = ''
   // console.table(hand)
   // console.table(nick)
@@ -112,16 +118,28 @@ function moveCardFunc() {
   this.classList1
   
   if (this.classList[1] === "card1") {
+    placeType = '.' + type[0]
+    document.querySelector(placeType).src = moveSRC
     card1 = 0
   } else if (this.classList[1] === "card2") {
+    placeType = '.' + type[1]
+    document.querySelector(placeType).src = moveSRC
     card2 = 0
   } else if (this.classList[1] === "card3") {
+    placeType = '.' + type[2]
+    document.querySelector(placeType).src = moveSRC
     card3 = 0
   } else if (this.classList[1] === "card4") {
+    placeType = '.' + type[3]
+    document.querySelector(placeType).src = moveSRC
     card4 = 0
   } else if (this.classList[1] === "card5") {
+    placeType = '.' + type[4]
+    document.querySelector(placeType).src = moveSRC
     card5 = 0
   } else if (this.classList[1] === "card6") {
+    placeType = '.' + type[5]
+    document.querySelector(placeType).src = moveSRC
     card6 = 0    
   }
 }
